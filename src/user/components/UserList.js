eb392a4b70260d70bcd1d4afc1ec3e3f85a2cb37
Madison Hung <51008147+madihung@users.noreply.github.com>
2024-01-17 21:05:41 -0800

    added navigation js and css files

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './UserList.css';
+import Card from '../../shared/components/UIElements/Card';
 import UserItem from './UserItem';
 
 const UserList = props => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-            <h2>No users found.</h2>
+                <Card>  
+                    <h2>No users found.</h2>
+                </Card>
         </div>
         );
     }
