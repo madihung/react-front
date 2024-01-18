8c5fc0357d36f86521e2085213a0bdcf2c42e26f
Madison Hung <51008147+madihung@users.noreply.github.com>
2024-01-17 21:14:34 -0800

    created MainNavigation and MainHeader components

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './MainNavigation.css'
+import MainHeader from "./MainHeader";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+
+const MainNavigation = props => {
+    return <MainHeader>
+        <button className="main-navigation__btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <button className="main-navigation__title">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+            <Link to="/">
+            Your Places
+            </Link>
+        </h1>
+        <nav>
+            ...
+        </nav>
+    </MainHeader>
+};
+
+export default MainNavigation;
